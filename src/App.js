e7f96c04c04b0c268f5223609407ad31eb64eb3f
alexkimeu0 <kim3ual3x@gmail.com>
2020-06-09 16:34:15 +0300

    pulled video data from the API & displayed it

--- src/App.js
+++ src/App.js
@@ -7,18 +7,29 @@ import { SearchBar, VideoDetail } from "./components";
 import youtube from "./api/youtube";
 
 class App extends React.Component {
+  state = {
+    videos: [],
+    selectedVideo: null,
+  };
+
   handleSubmit = async (searchTxt) => {
     const response = await youtube.get("search", {
       params: {
         part: "snippet",
         maxResults: 5,
-        key: "APIKEY",
+        key: "AIzaSyAoyR-57oPSUu49JibDEDrGx1A_ZnORLD4",
         q: searchTxt,
       },
+    });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
     });
   };
 
   render() {
+    const { selectedVideo } = this.state;
     return (
       <Grid justify="center" container spacing={10}>
         <Grid item xs={12}>
@@ -27,7 +38,7 @@ class App extends React.Component {
               <SearchBar formSubmit={this.handleSubmit} />
             </Grid>
             <Grid item xs={8}>
-              <VideoDetail />
+              <VideoDetail video={selectedVideo} />
             </Grid>
             <Grid item xs={4}>
               {/* VIDEO LIST */}
